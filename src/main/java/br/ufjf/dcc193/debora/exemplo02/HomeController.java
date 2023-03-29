8c61efcbbf098eacdabc420a95b92aaedf6a698a
dduartevla <deboraduarte@ice.ufjf.br>
2023-03-28 22:22:29 -0300

    Adiciono um home jsp

--- src/main/java/br/ufjf/dcc193/debora/exemplo02/HomeController.java
+++ src/main/java/br/ufjf/dcc193/debora/exemplo02/HomeController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Controller
 public class HomeController {
 
     @GetMapping({"/","/index.html"})
-    @ResponseBody
-    public String index(){
-        return "Hello, wolrd!";
+    public String index(HttpServletRequest req, HttpServletResponse resp){
+        //String nome = req.getParameter("nome");
+        //if (nome == null){
+        //    nome = "Desconhecido";
+        //}
+        return "/WEB-INF/views/home.jsp";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,15 @@
+<%@page pageEncoding="UTF-8%">
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo02: Home</title>
+</head>
+<body>
+    <h1> Exemplo02: Home </h1>
+    <p> Bem vindo(a) </p>
+    <p> Envie dados via <a href="formulario.html">formulário</a></p>
+</body>
+</html>
