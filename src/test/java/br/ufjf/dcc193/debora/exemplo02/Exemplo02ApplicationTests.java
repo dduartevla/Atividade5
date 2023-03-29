919a684650cde174197c9c9389d09a81cb042613
dduartevla <deboraduarte@ice.ufjf.br>
2023-03-28 21:37:43 -0300

    Commit inicial

---
+++ src/test/java/br/ufjf/dcc193/debora/exemplo02/Exemplo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.debora.exemplo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
