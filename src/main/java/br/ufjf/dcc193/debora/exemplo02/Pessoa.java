5b1764edb0215fe748c94b59604647534e68b487
dduartevla <deboraduarte@ice.ufjf.br>
2023-04-11 21:54:45 -0300

    Adiciona JPA e anota Pessoa

--- src/main/java/br/ufjf/dcc193/debora/exemplo02/Pessoa.java
+++ src/main/java/br/ufjf/dcc193/debora/exemplo02/Pessoa.java
@@ -1,20 +1,51 @@
 package br.ufjf.dcc193.debora.exemplo02;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pessoa {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     private String nome;
-    private String idade;
+    private Integer idade;
+
     
+    
+    public Pessoa() {
+        this(null,null);
+    }    
+
+    public Pessoa(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [id=" + id + ", nome=" + nome + ", idade=" + idade + "]";
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getIdade() {
-        return idade;
-    }
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
+
     
 }
